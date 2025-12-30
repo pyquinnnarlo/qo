19a31c35102ad6f20526f0fe2bd9c24220e30c10
Brilliant Narlo (Quinn) <105549100+pyquinnnarlo@users.noreply.github.com>
2025-12-30 13:00:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-pip uninstall opencv-python opencv-contrib-python -y
-python -m venv --system-site-packages venv
-source venv/bin/activate
-pip install face_recognition Flask SpeechRecognition pyaudio
+(venv) fyplg@FYPLG:~/Desktop/pi/exam $ python main.py 
+ [DB] Loading Student Faces...
+   + Loaded: john_doe
+ [DB] System Ready. Known students: 1
+ [VISION] Attempting to start camera via GStreamer...
+ [ERROR] Failed to open camera! Check if 'gstreamer1.0-libcamera' is installed.
+ * Serving Flask app 'main'
+ * Debug mode: off
+
